e82a129757fe026312b680ec87d25cb48483d6b2
Dustin J. Mitchell <dustin@mozilla.com>
2015-04-24 22:55:00 -0400

    add and use a testutil package

--- src/mig/modules/example/example_test.go
+++ src/mig/modules/example/example_test.go
@@ -1,8 +1,7 @@
-package example_test
+package example
 
 import (
-	"encoding/json"
-	"mig/modules/example"
+	"mig/testutil"
 	"testing"
 )
 
@@ -22,7 +21,7 @@ func TestValidateParameters(t *testing.T) {
 		{"http://mozilla.org", false},
 	}
 
-	r := new(example.Runner)
+	r := new(Runner)
 
 	for _, vt := range hostnames {
 		r.Parameters.LookupHost = vt.LookupHost
@@ -35,27 +34,8 @@ func TestValidateParameters(t *testing.T) {
 	}
 }
 
-func tryRun(t *testing.T, r *example.Runner) (err error) {
-	args, err := json.Marshal(r.Parameters)
-	t.Logf("running module with args %s", string(args))
-	if err != nil {
-		return err
-	}
-	res := []byte(r.Run(args))
-	return json.Unmarshal(res, &r.Results)
-}
-
 func TestRunInvalidLookupHost(t *testing.T) {
-	r := new(example.Runner)
-	r.Parameters.LookupHost = "in valid"
-	err := tryRun(t, r)
-	if err != nil {
-		t.Error(err)
-	}
-	if r.Results.Success {
-		t.Error("unexpected run success")
-	}
-	if len(r.Results.Errors) != 1 {
-		t.Error("no error response")
-	}
+	res := testutil.RunModule(t, new(Runner), params{false, false, "in valid"})
+	testutil.AssertModuleError(t, res,
+		"ValidateParameters: LookupHost parameter is not a valid FQDN.")
 }
---
+++ src/mig/testutil/modtest.go
@@ -0,0 +1,60 @@
+package testutil
+
+import (
+    "testing"
+    "mig"
+    "encoding/json"
+)
+
+func unexpectedErrors(t *testing.T, errors []string) {
+    for _, msg := range errors {
+        t.Errorf("Unexpected error: %s", msg)
+    }
+}
+
+func RunModule(t *testing.T, m mig.Moduler, args interface{}) mig.ModuleResult {
+    var marshalled_args []byte
+    marshalled_args, err := json.Marshal(args)
+    if (err != nil) {
+        t.Error(err)
+    }
+
+    t.Logf("Running module with args %s", marshalled_args)
+    marshalled_res := []byte(m.Run(marshalled_args))
+    t.Logf("Result: %s", marshalled_res)
+
+    var res mig.ModuleResult
+    err = json.Unmarshal(marshalled_res, &res)
+    if (err != nil) {
+        t.Error(err)
+    }
+    return res
+}
+
+func AssertModuleSucceeded(t *testing.T, res mig.ModuleResult) {
+    if (len(res.Errors) > 0) {
+        unexpectedErrors(t, res.Errors)
+        t.FailNow()
+    }
+    if !res.Success {
+        t.Fatal("Not successful")
+    }
+}
+
+func AssertModuleError(t *testing.T, res mig.ModuleResult, exp_error string) {
+    if res.Success {
+        t.Fatal("Unexpected success")
+    }
+    if len(res.Errors) == 0 {
+        t.Fatal("No errors")
+    }
+    if len(res.Errors) > 1 {
+        unexpectedErrors(t, res.Errors)
+        t.FailNow()
+    }
+
+    if res.Errors[0] != exp_error {
+        unexpectedErrors(t, res.Errors)
+        t.FailNow()
+    }
+}
